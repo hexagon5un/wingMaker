50a0cdba4f58e96e6946a2e3736aeb60b702c74f
Elliot Williams <elliot@pre.postero.us>
2020-12-17 16:47:54 +0100

    shortened heights, lessened travel
    it was just taking too long, was too boring.

--- kerf_test.py
+++ kerf_test.py
@@ -3,8 +3,8 @@
 import sys
 from gcodeWriter import GcodeWriter
 
-height = 100 ## tall enough to clear your foam scrap
 depth = 0    ## will cut to this depth: if you zero on the plate, use a positive number here
+height = 30 + depth ## tall enough to clear your foam scrap
 notch = 20   ## makes notches/gaps that are this wide
 speeds = sys.argv[1:]
 
@@ -14,8 +14,8 @@ g.travel(0, height, 0, height)
 for i, speed in enumerate(speeds):
     g.set_speed(speed)
     g.travel((i*2+1)*notch, height, (i*2+1)*notch, height)
-    g.move((i*2+1)*notch, 0, (i*2+1)*notch, 0)
-    g.move((i*2+2)*notch, 0, (i*2+2)*notch, 0)
+    g.move((i*2+1)*notch, depth, (i*2+1)*notch, depth)
+    g.move((i*2+2)*notch, depth, (i*2+2)*notch, depth)
     g.move((i*2+2)*notch, height, (i*2+2)*notch, height)
 
 g.travel(0, height, 0, height)
